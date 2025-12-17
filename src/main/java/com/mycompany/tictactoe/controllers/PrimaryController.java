bbb9b8ccb6c575af7760201d69c070064cd2159d
Ahmed Mohamed <ahmedmonatah@gmail.com>
2025-12-17 20:30:16 +0200

    Revert "Merge pull request #8 from AhmedMonatah/register-gui"
    This reverts commit b8ced7d652a44288e4113fec637f577a6f590692, reversing
    changes made to 9df0a5c532e5137e0ce9ee16108636381c30951c.

--- src/main/java/com/mycompany/tictactoe/controllers/PrimaryController.java
+++ src/main/java/com/mycompany/tictactoe/controllers/PrimaryController.java
@@ -3,18 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package com.mycompany.tictactoe.controllers;
-
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.fxml.FXML;
 
 /**
  * FXML Controller class
  *
- * @author Ahmed
+ * @author LENOVO
  */
 public class PrimaryController implements Initializable {
 
+
+    @FXML
+    private TextField username;
+    @FXML
+    private TextField userPassword;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Text registerPage;
     /**
      * Initializes the controller class.
      */
@@ -23,4 +39,20 @@ public class PrimaryController implements Initializable {
         // TODO
     }    
     
+    @FXML
+    private void usernameTextField(ActionEvent event) {
+    }
+
+    @FXML
+    private void userPasswordTextField(ActionEvent event) {
+    }
+
+    @FXML
+    private void loginButton(ActionEvent event) {
+    }
+
+    @FXML
+    private void toRegisterPage(MouseEvent event) {
+    }
+
 }
