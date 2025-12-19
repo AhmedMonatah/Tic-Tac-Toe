316d14ddb47a7497fc4c494516f6ccb661e9428e
sherryahmos473 <sherry.ahmos@gmail.com>
2025-12-19 22:20:26 +0200

    update game play

--- src/main/java/com/mycompany/tictactoe/controllers/GameplayController.java
+++ src/main/java/com/mycompany/tictactoe/controllers/GameplayController.java
@@ -6,10 +6,12 @@ package com.mycompany.tictactoe.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 /**
  * FXML Controller class
@@ -27,6 +29,12 @@ public class GameplayController implements Initializable {
     private Label aiScore;
     @FXML
     private GridPane gameGrid;
+    @FXML
+    private Label title;
+    @FXML
+    private Label player1;
+    @FXML
+    private Label player2;
     /**
      * Initializes the controller class.
      */
@@ -34,5 +42,41 @@ public class GameplayController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void btn00(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn10(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn20(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn01(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn11(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn21(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn02(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn12(ActionEvent event) {
+    }
+
+    @FXML
+    private void btn22(ActionEvent event) {
+    }
     
 }
