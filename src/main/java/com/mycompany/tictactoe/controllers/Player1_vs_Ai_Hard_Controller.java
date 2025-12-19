3b31bb6674b811a6b608dfd13f2e451d5531a05d
Ahmed Mohamed <ahmedmonatah@gmail.com>
2025-12-19 17:29:23 +0200

    add view for playervsplayer2 and playervsai

---
+++ src/main/java/com/mycompany/tictactoe/controllers/Player1_vs_Ai_Hard_Controller.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.tictactoe.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+/**
+ * FXML Controller class
+ *
+ * @author Ahmed
+ */
+public class Player1_vs_Ai_Hard_Controller implements Initializable {
+
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private TextField player1Input;
+    @FXML
+    private CheckBox recordCheckbox;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void handleBack(MouseEvent event) {
+    }
+
+}
